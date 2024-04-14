3d42786e77626b6dfb9cc2294421d5a1714e76cc
b3yb45 <beybaset@gmail.com>
2024-04-14 16:25:28 +0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,6 +4,7 @@ class Apartment():
         self.__type = type
         self.__capacity = capacity
         self.__comfort = comfort
+        self.__price_per_person = 0.0
 
     @property
     def number(self):
@@ -20,9 +21,23 @@ class Apartment():
     @property
     def comfort(self):
         return self.__comfort
+    
+    @property
+    def price_per_person(self):
+        return self.__price_per_person
 
+    @price_per_person.setter
+    def price_per_person(self, value):
+        self.__price_per_person = value
 
 class Hotel(Apartment):
+    __type_price_per_person = {
+        "одноместный": 2900.00,
+        "двухместный": 2300.00,
+        "полулюкс": 3200.00,
+        "люкс": 4100.00
+    }
+
     def __init__(self, apartments_file):
         self.__apartmnents = self.__load_apartments(apartments_file)
 
@@ -30,3 +45,7 @@ class Hotel(Apartment):
     def __load_apartments(file):
         with open(file) as f:
             return [Apartment(*[int(x) for x in line.split()]) for line in f]
+    
+    def set_price_per_person(self):
+        for apartment in self.__apartmnents:
+            apartment.price_per_person = self.__type_price_per_person[apartment.type]
